a913f70e368d1ed77a4065c6f254d7ac72e557b7
n.zhekov <n.zhekov@primegroup.ru>
2024-06-30 16:33:17 +0300

    Create repository

---
+++ src/main/java/adik/tasksfromleetcode/TasksFromLeetCodeApplication.java
@@ -0,0 +1,13 @@
+package adik.tasksfromleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksFromLeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksFromLeetCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/adik/tasksfromleetcode/TasksFromLeetCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package adik.tasksfromleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksFromLeetCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
